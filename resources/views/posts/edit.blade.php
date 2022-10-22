c1b55985fe21cd166ec94b90e38f634cf59e49cc
Syun1986 <syun990806@gmail.com>
2022-10-22 23:29:55 +0900

    マイグレーション

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -18,10 +18,12 @@
     	        <div class="content__title">
     	            <h2>Title</h2>
     	            <input type="text" name="post[title]" value="{{ $post->title }}"/>
+    	            <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
     	        </div>
     	        <div class="content__body">
     	            <h2>Body</h2>
-    	            <input type="text" name="post[body]" value="{{ $post->body }}"/>
+    	            <textarea name="post[body]">{{ $post->body }}</textarea>
+		            <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
     	        </div>
     	        <input type="submit" value="update"/>
     	    </form>
